32ea8174405131e1f0c72e77bcb192b567bb9abf
Daniel Loughry <loughry_d@yahoo.com>
2018-06-05 19:30:53 -0400

    Add project files.

---
+++ HDShowroomInventorySystem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HDShowroomInventorySystem
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
